83dca6b0585747eb67ff85cb14f4a86e0fb2e47b
GANGGMEMBERZIZI <cossnb@163.com>
2026-09-09 19:46:30 +0800

    feat(Main):add Error handle

--- app/main.hs
+++ app/main.hs
@@ -1,5 +1,7 @@
 import System.IO
 import System.Directory
+import System.IO.Error
+import Control.Exception
 import System.FilePath
 import GHC.IO.Encoding
 import Data.Typeable (typeOf)
@@ -8,22 +10,30 @@ import Types
 import Parser
 import Render
 import Scan
-main=do
+main=toTry `catch` handler
+toTry=do
     setLocaleEncoding utf8
-    let inputdir="./input"            
+    let inputdir="./input"
+    createDirectoryIfMissing True "input"           
     allFiles<-listDirectory inputdir
     let nameFiles=filter (isExtensionOf ".md") allFiles
-    contentFiles<-mapM (\name->do
-        let path=inputdir </> name
-        readFile' path 
-        ) nameFiles
-    -- print contentFiles    
-    let token=map scanToken contentFiles
-    -- print token
-    let astToken=map blockParser (map blockCut token)
-    -- print astToken   
-    forM_ (zip nameFiles astToken) $ \(name,ast)->
-        htmlRender name ast
-    putStrLn "编译完成"                
+    if null nameFiles then putStrLn "input里没文件"
+    else do
+            contentFiles<-mapM (\name->do
+                let path=inputdir </> name
+                readFile' path 
+                ) nameFiles    
+            let token=map scanToken contentFiles
+            -- print token
+            let astToken=map blockParser (map blockCut token)
+            -- print astToken
+            createDirectoryIfMissing True "output"  
+            forM_ (zip nameFiles astToken) $ \(name,ast)->
+                htmlRender name ast
+            putStrLn "编译完成"
+handler::IOError->IO()
+handler e
+   | isPermissionError e=putStrLn "权限有问题无法读取文件夹及文件"
+   | otherwise =ioError e                     
          
     
